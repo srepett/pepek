60632e83ef4c3af275d513e1d3cd1ebe598351e7
Zymzz <srepettsrepett85@gmail.com>
2025-11-27 20:21:09 +0800

    Create data_file_script.js

---
+++ Database/data_file_script.js
@@ -0,0 +1,7 @@
+// Mapping Link Download
+export const DOWNLOAD_LINKS = {
+    // Format: "Hero_SkinName": "Link"
+    "Aamon_Default Skin": "https://files.catbox.moe/kn4sd6.zip",
+    
+    // Tambahkan link lain di sini
+};
